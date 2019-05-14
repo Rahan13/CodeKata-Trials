9e6e2f361184f94d627d7bff619bae5c8ec59abb
Rahan13 <50615681+Rahan13@users.noreply.github.com>
2019-05-14 14:45:00 +0530

    Update Trial1.java

--- Trial1.java
+++ Trial1.java
@@ -1,40 +1,23 @@
 import java.util.*;
-
-public class Trial1{
-   
-static int a;
-   
-static void getData(){
-    
+public class Trial1{   
+static int a;   
+static void getData(){   
 Scanner s =  new Scanner(System.in);
-
-a = s.nextInt();
-    
-s.close();
-    
+a = s.nextInt();    
+s.close();    
 }
     
-static void compute(){
-        
-if(a>0)
-        
-System.out.println("Positive");
-        
-else if(a==0)
-        
-System.out.println("Zero");
-        
-else
-        
-System.out.println("Negative");
-    
+static void compute(){       
+if(a>0)        
+System.out.println("Positive");        
+else if(a==0)        
+System.out.println("Zero");        
+else       
+System.out.println("Negative");    
 }
      
-public static void main(String []args){
-        
-getData();
-        
-compute();
-     
+public static void main(String []args){        
+getData();        
+compute();     
 }
 }
