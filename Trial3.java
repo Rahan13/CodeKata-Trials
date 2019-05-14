4d5b8987c29bca2faa9656ba9f04466a13c04e31
Rahan13 <50615681+Rahan13@users.noreply.github.com>
2019-05-14 15:28:43 +0530

    Add files via upload

---
+++ Trial3.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Trial3{
+    
+static String a;
+    
+static String str ="aeiou";
+    
+static void getData(){
+    
+Scanner s =  new Scanner(System.in);
+    
+a = s.next();
+    
+s.close();
+    
+}
+    
+static void compute(){
+        
+if(str.contains(a))
+        
+System.out.println("Vowel");
+        
+else
+        
+System.out.println("Consonant");
+    
+}
+     
+public static void main(String []args){
+        
+getData();
+        
+compute();
+     
+}
+}
