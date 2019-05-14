9436cca8504054eef771ed0d7a1cc33e32858709
Rahan13 <50615681+Rahan13@users.noreply.github.com>
2019-05-14 15:43:09 +0530

    Add files via upload

---
+++ Trial4.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Trial4{
+    
+static String a;
+    
+static String str ="abcdefghijklmnopqrstuvxyz";
+    
+static void getData(){
+    
+Scanner s =  new Scanner(System.in);
+    
+a = s.next();
+    
+s.close();
+    
+}
+    
+static void compute(){
+        
+a.toLowerCase();
+        
+if(str.contains(a))
+       
+System.out.println("Alphabet");
+       
+else
+        
+System.out.println("No");
+    
+}
+     
+public static void main(String []args){
+        
+getData();
+        
+compute();
+     
+}
+}
