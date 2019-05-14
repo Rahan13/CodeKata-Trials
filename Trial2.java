63592d105d725d290ef8087afe33cfa71f880c66
Rahan13 <50615681+Rahan13@users.noreply.github.com>
2019-05-14 15:03:46 +0530

    Add files via upload

---
+++ Trial2.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Trial2{
+    
+static int a;
+    
+static void getData(){
+    
+Scanner s =  new Scanner(System.in);
+    
+a = s.nextInt();
+    
+s.close();
+    
+}
+   
+static void compute(){
+        
+if(a%2==0)
+        
+System.out.println("Even");
+       
+else if(a%2!=0)
+        
+System.out.println("Odd");
+        
+else
+        
+System.out.println("Invalid");
+    
+}
+     
+public static void main(String []args){
+        
+getData();
+        
+compute();
+     
+}
+}
