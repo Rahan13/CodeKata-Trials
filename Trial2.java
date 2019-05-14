e6f5ec05822ad79237efa57073c63eb07e614514
Rahan13 <50615681+Rahan13@users.noreply.github.com>
2019-05-14 15:05:18 +0530

    Update Trial2.java

--- Trial2.java
+++ Trial2.java
@@ -1,40 +1,24 @@
 import java.util.*;
 
 public class Trial2{
-    
-static int a;
-    
-static void getData(){
-    
-Scanner s =  new Scanner(System.in);
-    
-a = s.nextInt();
-    
-s.close();
-    
+static int a;   
+static void getData(){   
+Scanner s =  new Scanner(System.in);   
+a = s.nextInt();    
+s.close();   
 }
    
-static void compute(){
-        
-if(a%2==0)
-        
-System.out.println("Even");
-       
-else if(a%2!=0)
-        
-System.out.println("Odd");
-        
-else
-        
-System.out.println("Invalid");
-    
+static void compute(){      
+if(a%2==0)     
+System.out.println("Even");     
+else if(a%2!=0)     
+System.out.println("Odd");     
+else       
+System.out.println("Invalid");   
 }
      
-public static void main(String []args){
-        
-getData();
-        
-compute();
-     
+public static void main(String []args){      
+getData();       
+compute();   
 }
 }
