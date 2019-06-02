214a489e883370d0696519e39dd8ad44f8feb0bb
Rahan13 <50615681+Rahan13@users.noreply.github.com>
2019-06-03 00:30:22 +0530

    Update PrimeInter.py

--- PrimeInter.py
+++ PrimeInter.py
@@ -1,8 +1,10 @@
 a= input().split()
 for i in range(int(a[0]),int(a[1])):
-    count = 0
+    count1 = 0
+    count2 = 0
     for j in range(1,i+1):
         if(i%j == 0):
-            count += 1
-    if count==2:
-        print(i, end = " ")
+            count1 += 1
+    if count1==2:
+        count2 += 1
+print(count2)
