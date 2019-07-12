4853d482c98b14f382ea093077b477d02bcf0678
Rahan13 <50615681+Rahan13@users.noreply.github.com>
2019-07-12 09:35:46 +0530

    Update Pattern.py

--- Pattern.py
+++ Pattern.py
@@ -1,28 +1,32 @@
 num=input()
-if num>=0:
-    print("Unexpected Exit!")
-else:
-    for i in range(num):
-        if i==0:
-            print(" ",end="")
-        for j in range(num-i):
-            print(" ",end="")
-        for k in range(i+1):
-            print(k+1,end=" ")
-        if i!=0:
-            print(i-(i-1))
-        else:
-            print("")
-    i=num-1
-    while(i>0):
-        if i==1:
-            print(" ",end="")
-        for j in range(num+1-i):
-            print(" ",end="")
-        for k in range(i):
-            print(k+1,end=" ")
-        if i!=1:
-            print(i-(i-1))
-        else:
-            print("")
-        i =i-1
+try:
+    num=int(num)
+    if num<=0:
+        print("Unexpected Exit!")
+    else:
+        for i in range(num):
+            if i==0:
+                print(" ",end="")
+            for j in range(num-i):
+                print(" ",end="")
+            for k in range(i+1):
+                print(k+1,end=" ")
+            if i!=0:
+                print(i-(i-1))
+            else:
+                print("")
+        i=num-1
+        while(i>0):
+            if i==1:
+                print(" ",end="")
+            for j in range(num+1-i):
+                print(" ",end="")
+            for k in range(i):
+                print(k+1,end=" ")
+            if i!=1:
+                print(i-(i-1))
+            else:
+                print("")
+            i =i-1
+except Exception:
+    print("Float value found!")
